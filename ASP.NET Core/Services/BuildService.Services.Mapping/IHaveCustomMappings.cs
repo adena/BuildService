a1a5c134bcd29b5c2b9833d7da3b779318a7579f
Neda <rognedateneva@gmail.com>
2019-07-30 13:10:16 +0300

    Initial commit

---
+++ ASP.NET Core/Services/BuildService.Services.Mapping/IHaveCustomMappings.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildService.Services.Mapping
+{
+    using AutoMapper;
+
+    public interface IHaveCustomMappings
+    {
+        void CreateMappings(IProfileExpression configuration);
+    }
+}
---
+++ ASP.NET Core/Services/BuildService.Services.Mapping/IMapTo.cs
@@ -0,0 +1,7 @@
+﻿namespace BuildService.Services.Mapping
+{
+    // ReSharper disable once UnusedTypeParameter
+    public interface IMapTo<T>
+    {
+    }
+}
