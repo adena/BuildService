a901d40d48aa080430412986477c14f69ddb894a
Neda <rognedateneva@gmail.com>
2019-08-15 22:15:49 +0300

    Added Administration, Categories and Surveys to navbar. Finished HttpPost for Create work.

--- Web/BuildService.Web/Controllers/HomeController.cs
+++ Web/BuildService.Web/Controllers/HomeController.cs
@@ -14,6 +14,11 @@
             return this.View();
         }
 
+        public IActionResult Administration()
+        {
+            return this.View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() => this.View();
     }
--- Web/BuildService.Web/Startup.cs
+++ Web/BuildService.Web/Startup.cs
@@ -41,7 +41,11 @@
             // Framework services
             // TODO: Add pooling when this bug is fixed: https://github.com/aspnet/EntityFrameworkCore/issues/9741
             services.AddDbContext<ApplicationDbContext>(
-                options => options.UseSqlServer(this.configuration.GetConnectionString("DefaultConnection")));
+                options =>
+                {
+                    options.UseSqlServer(this.configuration.GetConnectionString("DefaultConnection"));
+                    options.EnableSensitiveDataLogging();
+                });
 
             services
                 .AddIdentity<ApplicationUser, ApplicationRole>(options =>
